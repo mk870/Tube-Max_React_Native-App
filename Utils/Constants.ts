bf8cd359259236003fb393e5c3768cef80916742
Mkhululi Ndlovu <87315888+mk870@users.noreply.github.com>
2024-05-30 22:25:00 +0000

    created a youtube player for both movies and shows

--- Utils/Constants.ts
+++ Utils/Constants.ts
@@ -22,7 +22,7 @@ export const regular = "Roboto-regular";
 export const medium = "Roboto-medium";
 export const bold = "Roboto-large";
 export const unknown = "unknown";
-export const notAvailable = "not available"
+export const notAvailable = "not available";
 
 export const tmdbKey = process.env.TMD_API_KEY;
 export const youtubeKey = process.env.YOUTUBE_API_KEY;
--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ScreenWrapper from '~/HOCs/ScreenWrapper'
 import { Redirect } from 'expo-router'
 
-
 const RootRoute = () => {
   return (
-    <Redirect href={"/movies"}/>
+      <Redirect href={"/movies"}/>
   )
 }
 
